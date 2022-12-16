136b8b151e04fb52fe3dff0048dc8da8e2b39e89
Meka827 <javy.meka@gmail.com>
2022-12-15 20:12:16 -0600

    testing

--- client/app.js
+++ client/app.js
@@ -4,19 +4,31 @@ fetch("http://localhost:3000/patients")
     console.log(data);
 });
 
+fetch("http://localhost:3000/providers")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+});
+
+fetch("http://localhost:3000/screens")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data);
+});
 
 
-fetch("http://localhost:3000/patients", {
-    Method: 'POST',
-    Headers: {
-      Accept: 'application.json',
-      'Content-Type': 'application/json'
-    },
-    Body: {
-        "name": "Shameka",
-        "dob": "1998-08-27",
-        "height_in": 65,
-        "weight_lbs": 180
-      },
-    Cache: 'default'
-  })
+
+// fetch("http://localhost:3000/patients", {
+//     Method: 'POST',
+//     Headers: {
+//       Accept: 'application.json',
+//       'Content-Type': 'application/json'
+//     },
+//     Body: {
+//         "name": "Shameka",
+//         "dob": "1998-08-27",
+//         "height_in": 65,
+//         "weight_lbs": 180
+//       },
+//     Cache: 'default'
+//   })
